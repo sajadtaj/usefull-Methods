fe521cde7c5c6d91c06bf186581de540da9a60c3
Xirano <74400894+sajadtaj@users.noreply.github.com>
2023-03-04 10:44:38 +0330

    Update merge.py

--- merge.py
+++ merge.py
@@ -8,3 +8,11 @@ inner_merged_total = pd.merge(TradigMatch,
                                 how='left',
                                 on=['key']
                                 )
+
+
+## Merge many data frame with same length
+# firs we merge alldata with df_0 to set lenth of alldata 
+all_data =  df_0    
+for i in (df_1,df_2,       ,df_n):  
+    all_data = pd.merge(all_data,i, how='left',left_index=True, right_index=True )
+

5d6da48873c1862437b943b56b80d55b904cd45c
Xirano <74400894+sajadtaj@users.noreply.github.com>
2023-04-23 15:24:43 +0430

    modify map

--- map.py
+++ map.py
@@ -10,9 +10,10 @@ def myfunc(n):
 
     
 x = map(myfunc, ('apple', 'banana', 'cherry'))
-
+print(list(x))
 
 def myfunc(a, b):
     return a + b
 
 y = map(myfunc, ('apple', 'banana', 'cherry'), ('orange', 'lemon', 'pineapple'))
+print(list(x))

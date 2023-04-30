f313cdf6cede8977be6f75a64f25a3376509e682
Xirano <sajadtajco@gmail.com>
2023-04-30 09:56:04 +0430

    Modify Sheet-date-merge

--- excelSheet.py
+++ excelSheet.py
@@ -1,3 +1,4 @@
+
 #*------------------------------+
 #*        Excel Sheets          |
 #*------------------------------+
@@ -9,3 +10,12 @@ df1= pd.read_excel(r'D:\names.xlsx','نام شیت ')
 df2= pd.read_excel(r'D:\names.xlsx','20101231-20151231')
 df3 =pd.read_excel(r'D:\names.xlsx','20151231-20220523')
 df4 =pd.read_excel(r'D:\names.xlsx','خرداد1401')
+
+#*------------------+
+#*  Sheet names     |
+#*------------------+
+
+# Read All Sheets
+file_name = pd.ExcelFile('Data.xlsx')
+# Name of All Sheet
+Sheet_names = file_name.sheet_names
--- merge.py
+++ merge.py
@@ -16,3 +16,14 @@ all_data =  df_0
 for i in (df_1,df_2,       ,df_n):  
     all_data = pd.merge(all_data,i, how='left',left_index=True, right_index=True )
 
+
+
+
+
+
+## Merge Many DataFrame
+# DataFrame in Dict
+list_DataFrame =[df_1,df_2,df_3,df_4,df_5,df_6,df_7]
+all_data=list_DataFrame[0]
+for DATA in list_DataFrame[1:]:  
+    all_data = pd.merge(all_data,DATA, how='left',on=['columns_A'])

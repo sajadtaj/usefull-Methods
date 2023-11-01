b024899fab13ca7362c00805e05434cf9e538245
Xirano <sajadtajco@gmail.com>
2023-11-01 13:57:28 +0330

    Modify add value to column

---
+++ Set_Value_in_DateFrame.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Assuming Shasta is your DataFrame
+
+DF.loc[DF['Date'] == '2021-11-15', 'Spesific_Column'] = 12
--- convert str to time.py
+++ convert str to time.py
@@ -12,4 +12,22 @@ df.index= datetime.datetime.strptime(df.index, '%Y-%m-%d').date()
 
 # sum by Days 
 date_1 = datetime.datetime.strptime(start_date, "%m/%d/%y")
+
+
+
 end_date = date_1 + datetime.timedelta(days=10)
+
+
+#-------------------------------------------------------------------
+import jdatetime
+# Convert Columns
+# Convert 14020802 to  1402-08-02
+
+def convert_to_new_format(date):
+    year = int(str(date)[:4])
+    month = int(str(date)[4:6])
+    day = int(str(date)[6:8])
+    persian_date = jdatetime.date(year, month, day)
+    return persian_date.strftime("%Y-%m-%d")
+
+Shasta_2['shamsiTradeDate'] = Shasta_2['shamsiTradeDate'].apply(convert_to_new_format)
